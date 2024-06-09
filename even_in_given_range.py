844332a09a34c9c0c182ccc9e5e537cf6b85198c
Kavitha V <kavitha474.v@gmail.com>
2024-06-09 15:55:16 +0530

    Even numbers in given range

---
+++ even_in_given_range.py
@@ -0,0 +1,19 @@
+While Loop
+def one_to_n_even_nums_in_while(param1,param2):
+    counter = param1
+    while (counter <= param2):
+        if(counter % 2 == 0):
+            print(counter)
+        counter = counter + 1
+start_num = int(input())
+end_num = int(input())
+one_to_n_even_nums_in_while(start_num,end_num)
+
+For Loop
+def one_to_n_even_nums_in_for(param1,param2):
+    for each_num in range(param1,param2+1):
+        if(each_num % 2 == 0):
+            print(each_num)
+start_num = int(input())
+end_num = int(input())
+one_to_n_even_nums_in_for(start_num,end_num)
--- even_num.py
+++ even_num.py
@@ -1,17 +1,17 @@
-# Whille Loop
-# def one_to_n_even_nums_in_while(param1):
-#     counter = 1
-#     while (counter <= param1):
-#         if(counter % 2 == 0):
-#             print(counter)
-#         counter = counter + 1
-# number = int(input())
-# one_to_n_even_nums_in_while(number)
+While Loop
+def one_to_n_even_nums_in_while(param1):
+    counter = 1
+    while (counter <= param1):
+        if(counter % 2 == 0):
+            print(counter)
+        counter = counter + 1
+number = int(input())
+one_to_n_even_nums_in_while(number)
 
-# For Loop
-# def one_to_n_even_nums_in_for(param1):
-#     for each_num in range(1,param1+1):
-#         if(each_num % 2 == 0):
-#             print(each_num)
-# number = int(input())
-# one_to_n_even_nums_in_for(number)
+For Loop
+def one_to_n_even_nums_in_for(param1):
+    for each_num in range(1,param1+1):
+        if(each_num % 2 == 0):
+            print(each_num)
+number = int(input())
+one_to_n_even_nums_in_for(number)
