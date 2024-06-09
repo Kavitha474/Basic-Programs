7948755d184fe9e41d3cf288bf147b9f8f1d1b0c
Kavitha V <kavitha474.v@gmail.com>
2024-06-09 15:40:16 +0530

    Added code for Even num

---
+++ odd_num.py
@@ -0,0 +1,17 @@
+# While Loop
+# def even_nums_in_while(param1):
+#     counter = 1
+#     while counter <= number:
+#         if(counter % 2 != 0):
+#             print(counter)
+#         counter = counter + 1
+# number = int(input())
+# even_nums_in_while(number)
+
+# For Loop
+# def even_nums_in_for(param1):
+#     for each_num in range(1,param1):
+#         if(each_num % 2 != 0):
+#             print(each_num)
+# number = int(input())
+# even_nums_in_for(number)
