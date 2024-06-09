ffa978bd7d6ede73141d9ab5b716140f4cf6a7d3
Kavitha V <kavitha474.v@gmail.com>
2024-06-09 14:31:29 +0530

    Added code for n to 1 natural numbers in Python

---
+++ natural_nums_1_to_n.py
@@ -0,0 +1,18 @@
+# Using While Loop
+
+# def natural_nums_using_while(param1):
+#     counter = 1
+#     while (counter <= number):
+#         print(counter)
+#         counter = counter + 1
+# number = int(input())
+# natural_nums_using_while(number)
+
+
+# Using For Loop
+
+# def natural_nums_using_for(param1):
+#     for each_num in range(1,param1+1):
+#         print(each_num)
+# number = int(input())
+# natural_nums_using_for(number)
---
+++ natural_nums_n_to_1.py
@@ -0,0 +1,17 @@
+# For Loop
+# def n_to_one_natural_num_using_for(param1):
+#     for each_num in range(1,param1+1):
+#         result = param1
+#         print(result)
+#         param1 = param1 - 1
+# number = int(input())
+# n_to_one_natural_num_using_for(number)
+
+# While Loop 
+# def n_to_one_natural_num_using_while(param1):
+#     counter = 1
+#     while counter <= param1:
+#         print(param1)
+#         param1 = param1 - 1
+# number = int(input())
+# n_to_one_natural_num_using_while(number)
