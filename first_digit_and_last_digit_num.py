e5f7e09cc31472005efb1bad7fbb5a0bcc3ff49e
Kavitha V <kavitha474.v@gmail.com>
2024-06-10 22:35:37 +0530

    Added some changes

--- first_digit_and_last_digit_num.py
+++ first_digit_and_last_digit_num.py
@@ -1,6 +1,10 @@
-While Loop
-def last_digit_of_num(param1):
-    digit = param1 % 10
-    print(digit)
+def first_and_last_digit_of_num(number):
+    first_digit = number % 10
+    print(first_digit)
+    while number >= 10:
+        number = int (number/10)
+    last_digit = number % 10
+    print(last_digit)
+      
 number = int(input())
-last_digit_of_num(number)
+first_and_last_digit_of_num(number)
