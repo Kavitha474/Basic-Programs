aabf3c7aee8592fc78decccc64909cb8c31c039d
Kavitha V <kavitha474.v@gmail.com>
2024-06-11 15:44:13 +0530

    power of num

--- power_of_num.py
+++ power_of_num.py
@@ -1,5 +1,15 @@
+def power_of_number(number, power):
+    result = 1
+    while(power > 0):
+        result =  result * number
+        power = power - 1
+    return result
 
 number = int(input())
-power_of_num = int(input())
-result = number ** power_of_num
-print(result)
+power = int(input())
+if(power > 0):
+    result_power_of_number = power_of_number(number, power)
+else:
+    updated_result = power_of_number(number, -(power))
+    result_power_of_number = 1/updated_result
+print(result_power_of_number)
