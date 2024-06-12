19ba25d86d2120ee01b9a7483c2119cbf791366d
Kavitha V <kavitha474.v@gmail.com>
2024-06-12 22:33:49 +0530

    Inverted right angle traingle

---
+++ Solid-Patterns/inverted_right_traingle.py
@@ -0,0 +1,32 @@
+#Inverted Traingle 
+#without using "*" * n
+
+number = int(input())
+for row in range (number):
+    result = ""
+    for col in range (number):
+        result = result + "* "
+    print(result)
+    number = number - 1
+
+# while Loop 
+
+def Inverted_right_traingle(number):
+    counter = number
+    while counter > 0:
+        print("* " * counter)
+        counter = counter-1
+number = int(input())
+Inverted_right_traingle(number)
+
+#for Loop
+
+def Inverted_right_traingle(number):
+    count = number
+    for row in range(number):
+        result = "* " * count
+        count = count - 1
+        print(result)
+
+number = int(input())
+Inverted_right_traingle(number)
