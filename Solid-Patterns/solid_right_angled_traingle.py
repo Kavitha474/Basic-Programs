4fe3f7dea493c9c7803d6bc0392444717a95f1bc
Kavitha V <kavitha474.v@gmail.com>
2024-06-12 10:25:21 +0530

    Solid Right angled Traingle

---
+++ Solid-Patterns/solid_right_angled_traingle.py
@@ -0,0 +1,20 @@
+# Solid Right angled Traingle
+# While Loop
+
+def solid_right_angled_traingle(no_of_rows):
+    counter = 1
+    while counter <= no_of_rows:
+        output = ("* " * counter)
+        print(output)
+        counter = counter + 1
+no_of_rows = int(input())
+solid_right_angled_traingle(no_of_rows)
+
+# For Loop
+
+def solid_right_angled_traingle(no_of_rows):
+    for each_row in range(1,no_of_rows+1):
+        result = "* " * each_row
+        print(result)
+no_of_rows = int(input())
+solid_right_angled_traingle(no_of_rows)
