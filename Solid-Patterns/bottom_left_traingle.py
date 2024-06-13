7bd04285aec0eee85595e00062612345fafe64e1
Kavitha V <kavitha474.v@gmail.com>
2024-06-13 10:12:07 +0530

    bottom left traingle

---
+++ Solid-Patterns/bottom_left_traingle.py
@@ -0,0 +1,35 @@
+#Nested Loop 
+#bottom Left traingle
+
+number = int(input())
+
+for row in range(number):
+    result = ""
+    counter = number
+    while counter > 0:
+        result = result + "* "
+        counter = counter - 1
+    print(result)
+    number = number -1
+    
+#while Loop
+
+def bottom_left_traingle(number):
+    counter = number
+    while counter > 0:
+        result = "* "*counter
+        counter = counter - 1
+        print(result)
+    
+number = int(input())
+bottom_left_traingle(number)
+
+#For Loop
+def bottom_left_traingle(number):
+    for row in range (number):
+        result = "* "*number
+        number = number - 1
+        print(result)
+    
+number = int(input())
+bottom_left_traingle(number)
