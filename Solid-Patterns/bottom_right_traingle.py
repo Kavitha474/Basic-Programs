f89b5cafcd376340afc89a562562a6393dd49b57
Kavitha V <kavitha474.v@gmail.com>
2024-06-13 11:45:13 +0530

    Solid bottom Right Traingle

---
+++ Solid-Patterns/bottom_right_traingle.py
@@ -0,0 +1,31 @@
+#Bottom Right traingle
+
+#While loop 
+
+def bottom_right_traingle(number):
+  for row in range(number):
+        spaces = "  "
+        star = "* "
+        no_of_spaces = spaces * row
+        no_of_stars = star * number
+        result = no_of_spaces + no_of_stars 
+        print(result)
+        number = number - 1
+    
+number = int(input())
+bottom_right_traingle(number)
+
+# #for loop 
+
+def bottom_right_traingle(number):
+  for row in range(number):
+        spaces = "  "
+        star = "* "
+        no_of_spaces = spaces * row
+        no_of_stars = star * number
+        result = no_of_spaces + no_of_stars 
+        print(result)
+        number = number - 1
+    
+number = int(input())
+bottom_right_traingle(number)
