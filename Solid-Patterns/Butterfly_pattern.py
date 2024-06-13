61094cd7fbaf57d08012a97bcd5013ca450592dd
Kavitha V <kavitha474.v@gmail.com>
2024-06-13 13:09:59 +0530

    Solid butterfly Pattern

---
+++ Solid-Patterns/Butterfly_pattern.py
@@ -0,0 +1,18 @@
+number = int(input())
+for row in range(1,number+1):
+    spaces = "   "
+    star = " * "
+    left_traingle_back_spaces = spaces * (number - row)
+    right_traingle_front_spaces = spaces * (number - row)
+    no_of_stars = star * row
+    result = no_of_stars + left_traingle_back_spaces + right_traingle_front_spaces + no_of_stars
+    print(result)
+for row in range (number):
+        space = "   "
+        star = " * "
+        left_traingle_back_spaces = space * row
+        right_traingle_back_spaces = space * row
+        no_of_stars = star * number
+        result = no_of_stars + left_traingle_back_spaces + right_traingle_back_spaces + no_of_stars
+        print(result)
+        number = number -1
