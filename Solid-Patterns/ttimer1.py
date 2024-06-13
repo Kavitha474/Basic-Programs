7402249c47072261d472257a15f9cce0cff1b4af
Kavitha V <kavitha474.v@gmail.com>
2024-06-13 17:23:23 +0530

    Solid timer1 pattern

---
+++ Solid-Patterns/ttimer1.py
@@ -0,0 +1,41 @@
+'''
+
+    *               * 
+    * *           * * 
+    * * *       * * * 
+    * * * *   * * * * 
+    * * * * * * * * * 
+    * * * *   * * * * 
+    * * *       * * * 
+    * *           * * 
+    *               * 
+
+
+'''
+
+def timer1(number):
+    for row in range(number):
+        space = "  "
+        star = "* "
+        left_traingle_back_spaces = space* (number - row)
+        right_traingle_front_spaces = space * (number - (row+1))
+        no_of_stars = star * (row + 1)
+        result = no_of_stars + left_traingle_back_spaces + right_traingle_front_spaces + no_of_stars
+        print(result)
+
+    for row in range(1):
+        star = "* "
+        result = star * ((2*number)+1)
+        print(result)
+    
+    for row in range(number):
+        space = "  "
+        star = "* "
+        left_traingle_back_spaces = space * (row+1)
+        right_traingle_front_spaces = space * (row)
+        no_of_stars = star * (number - row)
+        result = no_of_stars + left_traingle_back_spaces + right_traingle_front_spaces + no_of_stars
+        print(result)
+
+number = int(input())
+timer1(number)
