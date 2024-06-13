02625858dbda446c5d6ce565b39d5f63c7c409f1
Kavitha V <kavitha474.v@gmail.com>
2024-06-13 12:47:59 +0530

    Solid M pattern

---
+++ Solid-Patterns/M_pattern.py
@@ -0,0 +1,15 @@
+# M Pattern 
+
+def M_pattern(number):
+    for row in range (number):
+        space = "   "
+        star = " * "
+        left_traingle_back_spaces = space * row
+        right_traingle_back_spaces = space * row
+        no_of_stars = star * number
+        result = no_of_stars + left_traingle_back_spaces + right_traingle_back_spaces + no_of_stars
+        print(result)
+        number = number -1
+
+number = int(input())
+M_pattern(number)
