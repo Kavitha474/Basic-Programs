24d20902cd5da0de0821ee025ce9d16735f62bb0
Kavitha V <kavitha474.v@gmail.com>
2024-06-13 20:57:30 +0530

    Solid number pattren5

---
+++ Solid-Patterns/Solid-number_patterns/number_pattern5.py
@@ -0,0 +1,20 @@
+'''
+        1 
+      1 2 
+    1 2 3 
+  1 2 3 4 
+1 2 3 4 5 
+
+'''
+
+def number_pattern1(number):
+    for row in range(1,number+1):
+        result = ""
+        for each_row in range(1,row+1):
+            result = result+ str(each_row) + " "
+        space = "  "
+        front_spaces = space * (number - row)
+        print(front_spaces + result)
+
+number = int(input())
+number_pattern1(number)
