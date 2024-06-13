179752031f5561c57fcc22d497e329ef30e31125
Kavitha V <kavitha474.v@gmail.com>
2024-06-13 12:09:38 +0530

    Solid daimond pattern

---
+++ Solid-Patterns/daimond_pattern.py
@@ -0,0 +1,19 @@
+# Daimond pattern
+
+number = int(input())
+
+for row in range (1,number+1):
+    space = "  "
+    star = " *  "
+    front_spaces = space * (number - row)
+    no_of_stars = star * row
+    result = front_spaces + no_of_stars
+    print(result)
+for row in range (1,number):
+    space = "  "
+    star = " *  "
+    front_spaces = space * (row)
+    no_of_stars = star * (number - 1)
+    result = front_spaces + no_of_stars
+    print(result)
+    number = number -1
