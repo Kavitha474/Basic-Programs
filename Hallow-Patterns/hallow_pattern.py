bc47460bcd942f1450989be583c3260ee90b444e
Kavitha V <kavitha474.v@gmail.com>
2024-06-15 18:42:00 +0530

    Hallow Right Angle Triangle

---
+++ Hallow-Patterns/hallow_pattern.py
@@ -0,0 +1,13 @@
+def hallow_pattern(number):
+    print("_ " * (number+1))
+    
+    for each_row in range(1,number+1):
+        spaces= "  "
+        pipe = "| "
+        farword_slash = "/ "
+        mid_spaces = spaces * (number - each_row)
+        row = pipe + mid_spaces + farword_slash
+        print(row)
+
+number = int(input())
+hallow_pattern(number)
